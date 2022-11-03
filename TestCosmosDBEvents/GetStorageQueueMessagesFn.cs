1fae8f1c67949ed95365d8cff813cac412ffeb77
Raniel Garcia <20143627+micro9000@users.noreply.github.com>
2022-11-03 22:10:17 +0800

    Update GetStorageQueueMessagesFn.cs

--- TestCosmosDBEvents/GetStorageQueueMessagesFn.cs
+++ TestCosmosDBEvents/GetStorageQueueMessagesFn.cs
@@ -39,6 +39,8 @@ namespace TestCosmosDBEvents
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
     }
 
 }

4cb8e2aec76e7b3cdb6a8e121e8f9e4da4864e36
Raniel Garcia <20143627+micro9000@users.noreply.github.com>
2022-11-09 14:03:03 +0800

    Update Program.cs

--- MyWebAppTest/Program.cs
+++ MyWebAppTest/Program.cs
@@ -23,7 +23,11 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 string appConfigEndpoint = builder.Configuration["appConfigEndpoint"];
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
-    options.Connect(new Uri(appConfigEndpoint), new ManagedIdentityCredential());
+    options.Connect(new Uri(appConfigEndpoint), new ManagedIdentityCredential())
+        .ConfigureKeyVault(kv =>
+        {
+            kv.SetCredential(new DefaultAzureCredential());
+        });
 });
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
 
